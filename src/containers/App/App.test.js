ed639d31194527fd24ca6a4c05e6047fa24c7c88
Loryn Mason <masonloryn@gmail.com>
2019-01-08 11:57:11 -0700

    Add app test

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -3,6 +3,7 @@ import { fetchHouses } from '../../thunks/fetchHouses'
 
 jest.mock('../../thunks/fetchHouses')
 describe('App', () => {
+
   it('mapDispatchToProps calls dispatch with an fetchHouses thunks when fetchHouses is dipatched', async () => {
     const mockDispatch = jest.fn()
     const toDispatch = fetchHouses('some url')
@@ -11,5 +12,8 @@ describe('App', () => {
       mappedProps.fetchHouses('some url')
 
     expect(mockDispatch).toHaveBeenCalledWith(toDispatch)
+  })
+  it('should call fetchHouses on componetDidMount', () => {
+    
   })
 })
